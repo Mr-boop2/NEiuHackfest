44b7e3cab0a237c7aab46fd225e7cf9d1d441e09
Morpheus <123224358+Mr-boop2@users.noreply.github.com>
2025-04-04 11:36:23 -0500

    Update taskClass.java

--- taskClass.java
+++ taskClass.java
@@ -1,15 +1,24 @@
+import java.util.LinkedList;
+
 public class TaskClass {
+
     private String taskName; 
     private int taskPrio; 
     private int timeEst; 
     private int nextTask; 
+    private LinkedList<TaskClass> taskList; 
 
     public TaskClass(String taskName){
         this.taskName = taskName; 
         taskPrio = 0; 
         timeEst = 0; 
         nextTask = 0; 
+        taskList = new LinkedList<>(); 
     }
-    
+
+    public void addTask(TaskClass taskName){
+        taskList.add(taskName); 
+    }
+
 
 }
