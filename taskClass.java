10ab56186749e1cf10dd2717a91749a593e122e3
Morpheus <123224358+Mr-boop2@users.noreply.github.com>
2025-04-04 11:29:57 -0500

    Create taskClass.java

---
+++ taskClass.java
@@ -0,0 +1,15 @@
+public class TaskClass {
+    private String taskName; 
+    private int taskPrio; 
+    private int timeEst; 
+    private int nextTask; 
+
+    public TaskClass(String taskName){
+        this.taskName = taskName; 
+        taskPrio = 0; 
+        timeEst = 0; 
+        nextTask = 0; 
+    }
+    
+
+}
